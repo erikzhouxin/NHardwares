a14ded1e5b4b41a7126f0b1ddcf53e442d632359
ErikZhouXin@EZhouXin.com <ErikZhouXin@EZhouXin.com>
2021-10-06 10:57:04 +0800

    [ErikZhouXin]添加深坂科技R600的RFID读取操作项目

---
+++ ShenBanReader/R600MessageTran.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Data.ShenBanReader
+{
+    /// <summary>
+    /// 消息传送
+    /// </summary>
+    public class R600MessageTran
+    {
+        private byte btDataLen;        //数据包长度，数据包从‘长度’字节后面开始的字节数，不包含‘长度’字节本身
+        private byte btCheck;          //校验和，除校验和本身外所有字节的校验和
+
+        /// <summary>
+        /// 完整数据包
+        /// </summary>
+        public byte[] AryTranData { get; private set; }
+        /// <summary>
+        /// 数据包命令参数，部分命令无参数
+        /// </summary>
+        public byte[] AryData { get; private set; }
+        /// <summary>
+        /// 读写器地址
+        /// </summary>
+        public byte ReadId { get; private set; }
+        /// <summary>
+        /// 数据包命令代码
+        /// </summary>
+        public byte Cmd { get; private set; }
+        /// <summary>
+        /// 数据包头，默认为0xA0
+        /// </summary>
+        public byte PacketType { get; private set; }
+        /// <summary>
+        /// 构造
+        /// </summary>
+        public R600MessageTran()
+        {
+
+        }
+        /// <summary>
+        /// 构造
+        /// </summary>
+        /// <param name="btReadId"></param>
+        /// <param name="btCmd"></param>
+        /// <param name="btAryData"></param>
+        public R600MessageTran(byte btReadId, byte btCmd, byte[] btAryData)
+        {
+            int nLen = btAryData.Length;
+
+            this.PacketType = 0xA0;
+            this.btDataLen = Convert.ToByte(nLen + 3);
+            this.ReadId = btReadId;
+            this.Cmd = btCmd;
+
+            this.AryData = new byte[nLen];
+            btAryData.CopyTo(this.AryData, 0);
+
+            this.AryTranData = new byte[nLen + 5];
+            this.AryTranData[0] = this.PacketType;
+            this.AryTranData[1] = this.btDataLen;
+            this.AryTranData[2] = this.ReadId;
+            this.AryTranData[3] = this.Cmd;
+            this.AryData.CopyTo(this.AryTranData, 4);
+
+            this.btCheck = CheckSum(this.AryTranData, 0, nLen + 4);
+            this.AryTranData[nLen + 4] = this.btCheck;
+        }
+        /// <summary>
+        /// 构造
+        /// </summary>
+        /// <param name="btReadId"></param>
+        /// <param name="btCmd"></param>
+        public R600MessageTran(byte btReadId, byte btCmd)
+        {
+            this.PacketType = 0xA0;
+            this.btDataLen = 0x03;
+            this.ReadId = btReadId;
+            this.Cmd = btCmd;
+
+            this.AryTranData = new byte[5];
+            this.AryTranData[0] = this.PacketType;
+            this.AryTranData[1] = this.btDataLen;
+            this.AryTranData[2] = this.ReadId;
+            this.AryTranData[3] = this.Cmd;
+
+            this.btCheck = CheckSum(this.AryTranData, 0, 4);
+            this.AryTranData[4] = this.btCheck;
+        }
+        /// <summary>
+        /// 构造
+        /// </summary>
+        /// <param name="btAryTranData"></param>
+        public R600MessageTran(byte[] btAryTranData)
+        {
+            int nLen = btAryTranData.Length;
+
+            this.AryTranData = new byte[nLen];
+            btAryTranData.CopyTo(this.AryTranData, 0);
+
+
+            byte btCK = CheckSum(this.AryTranData, 0, this.AryTranData.Length - 1);
+            if (btCK != btAryTranData[nLen - 1])
+            {
+                return;
+            }
+
+            this.PacketType = btAryTranData[0];
+            this.btDataLen = btAryTranData[1];
+            this.ReadId = btAryTranData[2];
+            this.Cmd = btAryTranData[3];
+            this.btCheck = btAryTranData[nLen - 1];
+
+            if (nLen > 5)
+            {
+                this.AryData = new byte[nLen - 5];
+                for (int nloop = 0; nloop < nLen - 5; nloop++)
+                {
+                    this.AryData[nloop] = btAryTranData[4 + nloop];
+                }
+            }
+        }
+        /// <summary>
+        /// 检查和
+        /// </summary>
+        /// <param name="btAryBuffer"></param>
+        /// <param name="nStartPos"></param>
+        /// <param name="nLen"></param>
+        /// <returns></returns>
+        public byte CheckSum(byte[] btAryBuffer, int nStartPos, int nLen)
+        {
+            byte btSum = 0x00;
+
+            for (int nloop = nStartPos; nloop < nStartPos + nLen; nloop++)
+            {
+                btSum += btAryBuffer[nloop];
+            }
+
+            return Convert.ToByte(((~btSum) + 1) & 0xFF);
+        }
+    }
+}
---
+++ ShenBanReader/R600Talker.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace System.Data.ShenBanReader
+{
+    /// <summary>
+    /// 话事接口
+    /// </summary>
+    interface IR600Talker
+    {
+        /// <summary>
+        /// 接收到发来的消息
+        /// </summary>
+        event R600ReceivedEventHandler MessageReceived;
+        /// <summary>
+        /// 连接到服务端
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="port"></param>
+        /// <param name="strException"></param>
+        /// <returns></returns>
+        bool Connect(IPAddress ip, int port, out string strException);
+        /// <summary>
+        /// 发送数据包
+        /// </summary>
+        /// <param name="btAryBuffer"></param>
+        /// <returns></returns>
+        bool SendMessage(byte[] btAryBuffer);
+        /// <summary>
+        /// 注销连接
+        /// </summary>
+        void SignOut();
+        /// <summary>
+        /// 校验是否连接服务器
+        /// </summary>
+        /// <returns></returns>
+        bool IsConnect();
+    }
+    /// <summary>
+    /// 话事类
+    /// </summary>
+    public class R600Talker : IR600Talker
+    {
+        /// <summary>
+        /// 消息接收事件
+        /// </summary>
+        public event R600ReceivedEventHandler MessageReceived;
+
+        TcpClient client;
+        Stream streamToTran;
+        private Thread waitThread;
+
+        private bool bIsConnect = false;
+        /// <summary>
+        /// 连接
+        /// </summary>
+        /// <param name="ipAddress"></param>
+        /// <param name="nPort"></param>
+        /// <param name="strException"></param>
+        /// <returns></returns>
+        public bool Connect(IPAddress ipAddress, int nPort, out string strException)
+        {
+            strException = string.Empty;
+            try
+            {
+                client = new TcpClient();
+                client.Connect(ipAddress, nPort);
+                streamToTran = client.GetStream();    // 获取连接至远程的流
+
+                //建立线程收取服务器发送数据
+                ThreadStart stThead = new ThreadStart(ReceivedData);
+                waitThread = new Thread(stThead);
+                waitThread.IsBackground = true;
+                waitThread.Start();
+
+                bIsConnect = true;
+                return true;
+            }
+            catch (System.Exception ex)
+            {
+                strException = ex.Message;
+                return false;
+            }
+        }
+
+        private void ReceivedData()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] btAryBuffer = new byte[4096];
+                    int nLenRead = streamToTran.Read(btAryBuffer, 0, btAryBuffer.Length);
+                    if (nLenRead == 0)
+                    {
+                        continue;
+                    }
+
+                    if (MessageReceived != null)
+                    {
+                        byte[] btAryReceiveData = new byte[nLenRead];
+
+                        Array.Copy(btAryBuffer, btAryReceiveData, nLenRead);
+
+                        MessageReceived(btAryReceiveData);
+                    }
+                }
+                catch (System.Exception ex)
+                {
+
+                }
+            }
+
+        }
+        /// <summary>
+        /// 发送
+        /// </summary>
+        /// <param name="btAryBuffer"></param>
+        /// <returns></returns>
+        public bool SendMessage(byte[] btAryBuffer)
+        {
+            try
+            {
+                lock (streamToTran)
+                {
+                    streamToTran.Write(btAryBuffer, 0, btAryBuffer.Length);
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// 退出
+        /// </summary>
+        public void SignOut()
+        {
+            if (streamToTran != null)
+                streamToTran.Dispose();
+            if (client != null)
+                client.Close();
+
+            waitThread.Abort();
+            bIsConnect = false;
+        }
+        /// <summary>
+        /// 是连接
+        /// </summary>
+        /// <returns></returns>
+        public bool IsConnect()
+        {
+            return bIsConnect;
+        }
+    }
+}
